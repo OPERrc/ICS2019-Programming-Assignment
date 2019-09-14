491703a2246e8ee015230ad88755d7829e53f00a
tracer-ics2019 <tracer@njuics.org>
2019-09-14 03:48:20 -0400

    >  compile 181220041 operrc Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  03:48:20 up 1 day, 11:32,  6 users,  load average: 0.00, 0.06, 0.08 b3a11571f22ac9fbf2bdea833c3563e6466a69b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -210,7 +210,7 @@ static int cmd_x(char *args) {
   }
 
   for (int i=0; i<num; i++)
-    printf("0x%-12x: 0x%x\n", expr+i, paddr_read(expr+i, 3));   
+    printf("0x%-12x: 0x%x\n", expr+i, paddr_read(expr+i, 1));   
   
   return 0;
 }
