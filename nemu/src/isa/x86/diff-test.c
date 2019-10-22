3a088114c6f0bd3115fca5ffe3b4dccbd3e2891c
tracer-ics2019 <tracer@njuics.org>
2019-10-22 17:07:52 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  17:07:52 up 5 days, 53 min, 18 users,  load average: 0.05, 0.08, 0.10 55ddbfa25fe3d69e5b95c4d13e1c4d52604e494

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,9 +2,17 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  // if (ref_r->pc == pc)
-		// return true;
-	return false;
+	if (ref_r->eax != cpu.eax) return false;
+	if (ref_r->ecx != cpu.ecx) return false;
+	if (ref_r->edx != cpu.edx) return false;
+	if (ref_r->ebx != cpu.ebx) return false;
+	if (ref_r->esp != cpu.esp) return false;
+	if (ref_r->ebp != cpu.ebp) return false;
+	if (ref_r->esi != cpu.esi) return false;
+	if (ref_r->edi != cpu.edi) return false;
+	if (ref_r->pc != cpu.pc) return false;
+	if (ref_r->pc == pc) return false;
+	return true;
 }
 
 void isa_difftest_attach(void) {
