c97916bedfdd9f9124f07835299af77653a0c844
tracer-ics2019 <tracer@njuics.org>
2019-10-29 22:55:49 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  22:55:49 up  1:54,  3 users,  load average: 0.75, 0.32, 0.25 84f035be4b671894278d25aeab8961842b9e276f

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -64,8 +64,8 @@ int main(int argc, char *argv[]) {
 	// value_test();
 
   /* Receive commands from user. */
-  // is_batch_mode = false;
-	ui_mainloop(is_batch_mode);
+  is_batch_mode = false;
+  ui_mainloop(is_batch_mode);
 
   return 0;
 }
