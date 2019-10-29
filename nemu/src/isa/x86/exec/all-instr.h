860325eb7f20c75bf2a7864ac3f3e2bda2d5c1e3
tracer-ics2019 <tracer@njuics.org>
2019-10-29 22:10:53 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  22:10:53 up  1:09,  3 users,  load average: 0.33, 0.43, 0.41 75bf0adef21647305df8f2175ae42a3578b6214b

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -1,14 +1,23 @@
 #include "cpu/exec.h"
 
-make_EHelper(mov);
-make_EHelper(call);
-make_EHelper(xor);
-make_EHelper(ret);
+// arith.c
 make_EHelper(sub);
+
+// control.c
+make_EHelper(call);
+make_EHelper(ret);
+
+// data-mov.c
+make_EHelper(mov);
 make_EHelper(push);
+make_EHelper(lea);
+
+// logic.c
+make_EHelper(xor);
 
 make_EHelper(operand_size);
 
+// special.c
 make_EHelper(nop);
 make_EHelper(inv);
 make_EHelper(nemu_trap);
