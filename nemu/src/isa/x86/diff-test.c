bb9f0b48ce58ec06d5f2fe3efadd8af608dad849
tracer-ics2019 <tracer@njuics.org>
2019-10-29 22:20:25 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  22:20:25 up  1:19,  3 users,  load average: 0.26, 0.40, 0.42 e3517fb72a5e732a2ebf049e7d67cb4d765fc9dd

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,19 +2,43 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-	if (ref_r->eax != cpu.eax) return false;
-	if (ref_r->ecx != cpu.ecx) return false;
-	if (ref_r->edx != cpu.edx) return false;
-	if (ref_r->ebx != cpu.ebx) return false;
-	if (ref_r->esp != cpu.esp) return false;
-	if (ref_r->ebp != cpu.ebp) return false;
-	if (ref_r->esi != cpu.esi) return false;
-	if (ref_r->edi != cpu.edi) return false;
-	if (ref_r->pc != cpu.pc) return false;
-	if (ref_r->pc == pc) {
-		Log("pc wrong!\n");
+	if (ref_r->eax != cpu.eax) { 
+		Log("cpu.eax wrong! Should be 0x%x!\n", ref_r->eax);
 		return false;
 	}
+	if (ref_r->ecx != cpu.ecx) { 
+		Log("cpu.ecx wrong! Should be 0x%x!\n", ref_r->ecx);
+		return false;
+	}
+	if (ref_r->edx != cpu.edx) { 
+		Log("cpu.edx wrong! Should be 0x%x!\n", ref_r->edx);
+		return false;
+	}
+	if (ref_r->ebx != cpu.ebx) { 
+		Log("cpu.ebx wrong! Should be 0x%x!\n", ref_r->ebx);
+		return false;
+	}
+	if (ref_r->esp != cpu.esp) { 
+		Log("cpu.esp wrong! Should be 0x%x!\n", ref_r->esp);
+		return false;
+	}
+	if (ref_r->ebp != cpu.ebp) { 
+		Log("cpu.ebp wrong! Should be 0x%x!\n", ref_r->ebp);
+		return false;
+	}
+	if (ref_r->esi != cpu.esi) { 
+		Log("cpu.esi wrong! Should be 0x%x!\n", ref_r->esi);
+		return false;
+	}
+	if (ref_r->edi != cpu.edi) { 
+		Log("cpu.edi wrong! Should be 0x%x!\n", ref_r->edi);
+		return false;
+	}
+	if (ref_r->pc != cpu.pc) { 
+		Log("cpu.pc wrong! Should be 0x%x!\n", ref_r->pc);
+		return false;
+	}
+	if (ref_r->pc == pc) return false;
 	// if (ref_r->eflags.CF != cpu.eflags.CF) return false;
 	// if (ref_r->eflags.ZF != cpu.eflags.ZF) return false;
 	// if (ref_r->eflags.SF != cpu.eflags.SF) return false;
