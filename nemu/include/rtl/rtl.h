b8e63ce9fc3cfa07ceb4e22e938ae8843889e277
tracer-ics2019 <tracer@njuics.org>
2019-10-30 19:42:57 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  19:42:57 up  2:30,  5 users,  load average: 0.27, 0.15, 0.19 76d7423cc02afaee7c95aace11b0523813e70519

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -139,6 +139,8 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   if (width == 1 && *src1 >> 7 == 1)
     *dest = *src1 | 0xffffff00;
+  else if (width == 2 && *src1 >> 15 == 1)
+    *dest = *src1 | 0xffff0000;
   else
     *dest = *src1;
 }
