93a43fb6421cd47281ee6130a1a3c18139fba375
tracer-ics2019 <tracer@njuics.org>
2019-10-30 18:04:34 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  18:04:34 up 52 min,  3 users,  load average: 0.54, 0.38, 0.29 6653daffd5073baa549590b5d2026c836c9dc727

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -48,7 +48,7 @@ make_EHelper(cmp) {
   // update CF
   rtl_is_sub_carry(&s1, &s0, &id_dest->val);
   rtl_set_CF(&s1);
-  // Log("cmp: ZF = %d, SF = %d\n", cpu.ZF, cpu.SF);
+  Log("ZF = %d, OF = %d, CF = %d, SF = %d\n", cpu.ZF, cpu.OF, cpu.CF, cpu.SF);
 
   print_asm_template2(cmp);
 }
