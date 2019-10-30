258adba79b79be04ab29a791a6a15d0a6d2ae15e
tracer-ics2019 <tracer@njuics.org>
2019-10-30 11:12:57 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  11:12:57 up 14:12,  3 users,  load average: 0.75, 0.78, 0.68 3c76f8aabcb36a52aefc043ea64336d7686b851d

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -17,6 +17,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_O:
     case CC_B:
     case CC_E: *dest = cpu.ZF == 1 ? 1 : 0; break;
+    case CC_NE: *dest = cpu.ZF == 0 ? 1 : 0; break;
     case CC_BE:
     case CC_S:
     case CC_L:
