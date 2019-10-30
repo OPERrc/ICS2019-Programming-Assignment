ac0de95cf9dfd04d28c6b92ace7884d9db4fa9a6
tracer-ics2019 <tracer@njuics.org>
2019-10-30 09:41:29 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  09:41:29 up 12:40,  3 users,  load average: 0.35, 0.39, 0.33 82deda0b3e87bf893b69af3a64f0f23ed4d06e74

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -1,18 +1,47 @@
 #include "nemu.h"
 #include "monitor/diff-test.h"
 
+#define make_isa_checkregs(name) \
+	void concat(checkregs_, name)() { \
+    if (ref_r->name != cpu.name) { \
+		  Log("Should be 0x%x!\n", ref_r->name); \
+		  flag = false; \
+	  } \
+  }
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+	bool flag = true;
+  make_isa_checkregs(eax);
+  make_isa_checkregs(ecx);
+  make_isa_checkregs(edx);
+  make_isa_checkregs(ebx);
+  make_isa_checkregs(esp);
+  make_isa_checkregs(ebp);
+  make_isa_checkregs(esi);
+  make_isa_checkregs(edi);
+  make_isa_checkregs(pc);
+  
+  checkregs_eax();
+  checkregs_ecx();
+  checkregs_edx();
+  checkregs_ebx();
+  checkregs_esp();
+  checkregs_ebp();
+  checkregs_esi();
+  checkregs_edi();
+  checkregs_pc();
+  /*
 	if (ref_r->eax != cpu.eax) { 
 		printf("cpu.eax wrong! Should be 0x%x!\n", ref_r->eax);
-		return false;
+		flag = false;
 	}
 	if (ref_r->ecx != cpu.ecx) { 
 		printf("cpu.ecx wrong! Should be 0x%x!\n", ref_r->ecx);
-		return false;
+		flag = false;
 	}
 	if (ref_r->edx != cpu.edx) { 
 		printf("cpu.edx wrong! Should be 0x%x!\n", ref_r->edx);
-		return false;
+		flag = false;
 	}
 	if (ref_r->ebx != cpu.ebx) { 
 		printf("cpu.ebx wrong! Should be 0x%x!\n", ref_r->ebx);
@@ -39,13 +68,14 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 		printf("original pc is 0x%x.\n", pc);
 		return false;
 	}
+  */
 	// if (ref_r->pc == pc) return false;
 	// if (ref_r->eflags.CF != cpu.eflags.CF) return false;
 	// if (ref_r->eflags.ZF != cpu.eflags.ZF) return false;
 	// if (ref_r->eflags.SF != cpu.eflags.SF) return false;
 	// if (ref_r->eflags.IF != cpu.eflags.IF) return false;
 	// if (ref_r->eflags.OF != cpu.eflags.OF) return false;
-	return true;
+	return flag;
 }
 
 void isa_difftest_attach(void) {
