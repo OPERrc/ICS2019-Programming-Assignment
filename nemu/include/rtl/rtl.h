d7256f7b382c3f6eb572f4785279c0725aae75a6
tracer-ics2019 <tracer@njuics.org>
2019-10-30 18:39:01 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  18:39:01 up  1:26,  3 users,  load average: 0.54, 0.42, 0.39 7bd1085b39f1234a6810b13f60e2a7bdb4e019ba

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -137,7 +137,10 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  if (width == 1 && *src1 >> 7 == 1)
+    *dest = *src1 | 0xffffff00;
+  else
+    *dest = *src1;
 }
 
 static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
