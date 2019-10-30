7fda86ec91a0b68ed5ba610cf8bcc6d5ec374e82
tracer-ics2019 <tracer@njuics.org>
2019-10-30 11:09:41 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  11:09:41 up 14:08,  3 users,  load average: 0.50, 0.67, 0.63 ed23e13d9e02584aa91eb1472b1f30c7e1f1be

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -5,7 +5,7 @@ make_EHelper(add) {
   operand_write(id_dest, &s0);
   // eflags TODO();
   rtl_update_ZFSF(&s0, id_dest->width);
-  printf("add: ZF = %d, SF = %d\n", cpu.ZF, cpu.SF);
+  Log("add: ZF = %d, SF = %d\n", cpu.ZF, cpu.SF);
   print_asm_template2(add);
 }
 
@@ -14,7 +14,7 @@ make_EHelper(sub) {
 	operand_write(id_dest, &s0);
 	// eflags TODO();
   rtl_update_ZFSF(&s0, id_dest->width);
-  printf("sub: ZF = %d, SF = %d\n", cpu.ZF, cpu.SF);
+  Log("sub: ZF = %d, SF = %d\n", cpu.ZF, cpu.SF);
   print_asm_template2(sub);
 }
 
@@ -22,13 +22,18 @@ make_EHelper(cmp) {
   rtl_sub(&s0, &id_dest->val, &id_src->val);
 
   rtl_update_ZFSF(&s0, id_dest->width);
-  printf("cmp: ZF = %d, SF = %d\n", cpu.ZF, cpu.SF);
+  Log("cmp: ZF = %d, SF = %d\n", cpu.ZF, cpu.SF);
 
   print_asm_template2(cmp);
 }
 
 make_EHelper(inc) {
-  TODO();
+  rtl_li(&s0, 1);
+  rtl_add(&s1, &id_dest->val, &s0);
+  operand_write(id_dest, &s1);
+  // eflags TODO();
+  rtl_update_ZFSF(&s0, id_dest->width);
+  Log("inc: ZF = %d, SF = %d\n", cpu.ZF, cpu.SF);
 
   print_asm_template1(inc);
 }
