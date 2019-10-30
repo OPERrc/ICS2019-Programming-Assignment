3015483984196e9d0194dcfc8ae3a067db8ebe3e
tracer-ics2019 <tracer@njuics.org>
2019-10-30 23:43:25 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  23:43:25 up  6:31,  5 users,  load average: 0.44, 0.35, 0.32 ea944bb13258236cb3a3b88654e86cf054a73162

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -43,6 +43,9 @@ static inline void rtl_pop(rtlreg_t* dest) {
 static inline void rtl_is_sub_overflow(rtlreg_t* dest,
     const rtlreg_t* res, const rtlreg_t* src1, const rtlreg_t* src2, int width) {
   // dest <- is_overflow(src1 - src2)
+  if (*src2 == 0x80000000)
+    *dest = 1;
+  else
   *dest = (*src1 >> (width * 8 - 1) == -(*src2) >> (width * 8 - 1) 
     && *src1 >> (width * 8 - 1) != *res >> (width * 8 - 1)) ? 1 : 0;
 }
