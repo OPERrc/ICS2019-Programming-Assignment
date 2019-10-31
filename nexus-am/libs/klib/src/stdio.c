39e986dcb20168d8bd467b5fc8ae57cddfa84090
tracer-ics2019 <tracer@njuics.org>
2019-10-31 10:16:18 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  10:16:18 up 17:03,  5 users,  load average: 0.40, 0.63, 0.71 49cd2130c89b70a96ebbe37c8d870450c9b42448

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -12,7 +12,7 @@ char *itoa(int value, char *str, int radix) {
     value /= radix;
     len++;
   }
-  str[len] = '\0';
+  //str[len] = '\0';
   for (int i = 0; i <= len / 2; i++){
     char tmp = str[i];
     str[i] = str[len - 1 - i];
