457064f5a4f0973b477f3f39e9f83b0f7665ad41
tracer-ics2019 <tracer@njuics.org>
2019-10-31 09:18:28 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  09:18:28 up 16:06,  5 users,  load average: 0.77, 0.72, 0.66 ca31792fc8b56102ff9cfde1b59b843cda547713

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,6 +3,24 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+char *itoa(int value, char *str, int radix) {
+  assert(radix > 16);
+  char index[] = "0123456789abcdef";
+  int len = 0;
+  while (value > 0) {
+    str[len] = index[value % radix];
+    value /= radix;
+    len++;
+  }
+  str[len] = '\0';
+  for (int i = 0; i <= len / 2; i++){
+    char tmp = str[i];
+    str[i] = str[len - 1 - i];
+    str[len - 1 - i] = tmp;
+  }
+  return str;
+}
+
 int printf(const char *fmt, ...) {
   return 0;
 }
@@ -12,6 +30,36 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+
+  va_start(ap, fmt);
+  int flag = 0;
+  while (*fmt)
+    switch (*fmt++) {
+      case '%': 
+        flag = 1;
+        break;
+      case 's':
+        if (flag) {
+          char *str = va_arg(ap, char *);
+          out = strcat(out, str);
+          flag = 0;
+          break;
+        }
+        else break;
+      case 'd':
+        if (flag) {
+          int d = va_arg(ap, int);
+          char str[1000];
+          out = strcat(out, itoa(d, str, 10));
+          flag = 0;
+          break;
+        }
+        else break;
+      default: break;
+    }
+  va_end(ap);
+
   return 0;
 }
 
