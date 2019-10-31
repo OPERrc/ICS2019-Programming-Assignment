42df7de69ffce5f40c9b30596551ede92c9008d5
tracer-ics2019 <tracer@njuics.org>
2019-10-31 18:51:48 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  18:51:48 up 1 day,  1:39,  5 users,  load average: 0.32, 0.42, 0.48 328452f0e8db9e87af700ad4b2ac929df763785a

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,6 +3,8 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+#define make_format \
+
 char *itoa(int value, char *str, int radix) {
   char index[] = "0123456789abcdef";
   int len = 0;
@@ -21,17 +23,73 @@ char *itoa(int value, char *str, int radix) {
 }
 
 int printf(const char *fmt, ...) {
-  TODO();
-  return 0;
+  va_list(ap);
+  va_start(ap, fmt);
+  char buf[1000];
+  char *str = buf;
+  vsprintf(str, fmt, ap);
+  va_end(ap);
+  int len = 0;
+  while (*str) {
+    _putc(*str);
+    str++;
+    len++;
+  }
+  return len;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  TODO();
+  char a[1000];
+  char *str = a;
+  char *tmp = out;
+  int d;
+
+  int flag = 0;
+  while (*fmt) {
+    if (*fmt != '%' && !flag) {
+      *tmp++ = *fmt;
+      fmt++;
+      continue;
+    }
+
+    flag = 1;
+    switch (*fmt) {
+      case 's':
+        str = va_arg(ap, char *);
+        while (*str) {
+          *tmp++ = *str;
+          str++;
+        }
+        flag = 0;
+        break;
+      
+      case 'd':
+        d = va_arg(ap, int);
+        str = itoa(d, str, 10);
+        while (*str) {
+          *tmp++ = *str;
+          str++;
+        }
+        flag = 0;
+        break;
+
+      case '%': break;
+
+      default: break;
+    }
+    fmt++;
+  }
+  *tmp = '\0';
+
   return 0;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list args;
+  va_list(ap);
+  va_start(ap, fmt);
+  vsprintf(out, fmt, ap);
+  va_end(ap);
+  /*va_list args;
   char a[1000];
   char *str = a;
   char *tmp = out;
@@ -72,7 +130,7 @@ int sprintf(char *out, const char *fmt, ...) {
     fmt++;
   }
   *tmp = '\0';
-  va_end(args);
+  va_end(args);*/
 
   return 0;
 }
