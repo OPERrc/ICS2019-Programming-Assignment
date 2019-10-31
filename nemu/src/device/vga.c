dd2f80d81997111da61b8fe31fc7bce106ab43f4
tracer-ics2019 <tracer@njuics.org>
2019-11-01 02:34:19 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  02:34:19 up 1 day,  9:22,  5 users,  load average: 0.47, 0.46, 0.47 7bba01ef1e89d3687d2be6058ed4539e415d21a6

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -30,8 +30,8 @@ static inline void update_screen() {
 
 static void vga_io_handler(uint32_t offset, int len, bool is_write) {
   // TODO: call `update_screen()` when writing to the sync register
-  if (is_write)
-    update_screen();
+  //if (is_write)
+    //update_screen();
   // screensize_port_base[1];
 }
 
