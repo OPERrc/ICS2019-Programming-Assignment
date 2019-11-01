25535187cb74ade0134cc70b1e968e5ac72c6d85
tracer-ics2019 <tracer@njuics.org>
2019-11-01 14:02:37 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  14:02:37 up 1 day, 20:50,  5 users,  load average: 1.24, 1.04, 0.83 2d8c271a63d46879f7e855ef407deba57b587c

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -26,7 +26,7 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-        uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+        static uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
         int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
         uint32_t *pixels = ctl->pixels;
         uint32_t W = screen_width(), H = screen_height();
