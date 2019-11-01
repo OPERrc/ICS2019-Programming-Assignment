db24b3d770582ecd0f6725bf50212de3d6d3ba8e
tracer-ics2019 <tracer@njuics.org>
2019-11-01 12:43:52 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  12:43:52 up 1 day, 19:31,  5 users,  load average: 0.43, 0.55, 0.52 7ce5d4faa9df27da44962d9ce067bac8e02d4a6

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -27,7 +27,7 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
-      //else {
+      else {
         uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
         int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
         uint32_t *pixels = ctl->pixels;
@@ -37,7 +37,7 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
         for (int j = 0; j < h && y + j < H; j ++) {
           memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
           pixels += w;
-        //}
+        }
       }
       return size;
     }
