592fe980c1ee455774bf473797788441c446f00e
tracer-ics2019 <tracer@njuics.org>
2019-11-01 20:14:55 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  20:14:55 up 2 days,  3:02,  5 users,  load average: 0.42, 0.52, 0.45 4e437d637b28b452b8ad97888246340733821a2b

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -45,6 +45,9 @@ static inline void rtl_pop(rtlreg_t* dest) {
 static inline void rtl_is_sub_overflow(rtlreg_t* dest,
     const rtlreg_t* res, const rtlreg_t* src1, const rtlreg_t* src2, int width) {
   // dest <- is_overflow(src1 - src2)
+  rtl_msb(&t0, src1, width);
+  rtl_msb(&t1, src2, width);
+  rtl_msb(dest, res, width);
   t0 = (*src1 >> (width * 8 - 1)) & 1;
   t1 = (~(*src2) >> (width * 8 - 1)) & 1;
   *dest = (*res >> (width * 8 - 1)) & 1;
@@ -67,8 +70,8 @@ static inline void rtl_is_add_overflow(rtlreg_t* dest,
   *dest = (*res >> (width * 8 - 1)) & 1;*/
   
   rtl_msb(&t0, src1, width);
-  rtl_msb(&t1, src1, width);
-  rtl_msb(dest, src1, width);
+  rtl_msb(&t1, src2, width);
+  rtl_msb(dest, res, width);
   *dest = (t0 == t1 && t0 != *dest) ? 1 : 0;
   /**dest = (*src1 >> (width * 8 - 1) == *src2 >> (width * 8 - 1) 
     && *src2 >> (width * 8 - 1) != *res >> (width * 8 - 1)) ? 1 : 0;*/
