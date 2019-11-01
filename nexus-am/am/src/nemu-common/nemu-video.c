5f16bc981d903f4f67d66d0a164a039b6d06cc3e
tracer-ics2019 <tracer@njuics.org>
2019-11-01 13:47:48 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  13:47:48 up 1 day, 20:35,  5 users,  load average: 0.83, 0.68, 0.67 c53e7f07e0348d018f589212f2167596f85dc860

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -38,7 +38,7 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
           pixels += w;
         }
       if (ctl->sync) {
-        //outl(SYNC_ADDR, 0);
+        outl(SYNC_ADDR, 1);
       }
       return size;
     }
