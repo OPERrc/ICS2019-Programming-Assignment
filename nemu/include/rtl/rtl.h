a6ca0813bb5c437132898c0e8f3e5e91f09cad0c
tracer-ics2019 <tracer@njuics.org>
2019-11-01 20:10:52 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  20:10:52 up 2 days,  2:58,  5 users,  load average: 0.69, 0.60, 0.44 b4f5e6ece01687c658a06488771012f77172783

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -132,8 +132,8 @@ void interpret_rtl_exit(int state, vaddr_t halt_pc, uint32_t halt_ret);
 
 static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
-  rtl_li(&t1, 0);
-  rtl_sub(&t1, 0, &t1);
+  rtl_li(&ir, 0);
+  rtl_sub(&t1, 0, &ir);
   rtl_subi(dest, &t1, 1);
 }
 
@@ -147,21 +147,7 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
     }
   else
     *dest = *src1;
-  
-  /*if (width == 1) {
-    if (*src1 >> 7 == 1)
-      *dest = *src1 | 0xffffff00;
-    else
-      *dest = *src1 & 0x000000ff;
-  }
-  else if (width == 2) {
-    if (*src1 >> 15 == 1)
-      *dest = *src1 | 0xffff0000;
-    else
-      *dest = *src1 & 0x0000ffff;
-  }
-  else
-    *dest = *src1;*/
+  // rtl_msb()
 }
 
 static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
@@ -178,7 +164,11 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 
 static inline void rtl_mux(rtlreg_t* dest, const rtlreg_t* cond, const rtlreg_t* src1, const rtlreg_t* src2) {
   // dest <- (cond ? src1 : src2)
-  TODO();
+  switch (*cond) {
+    case 1: rtl_mv(dest, src1);
+    case 0: rtl_mv(dest, src2);
+    default: assert(0);
+  }
 }
 
 #include "isa/rtl.h"
