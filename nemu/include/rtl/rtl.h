5524760684528ea44818824d1d9eda539d31e11e
tracer-ics2019 <tracer@njuics.org>
2019-11-01 20:37:35 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  20:37:35 up 2 days,  3:25,  5 users,  load average: 0.40, 0.56, 0.62 38b4129cb0231ff16d047834db86c1626a0fa926

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -132,37 +132,9 @@ void interpret_rtl_exit(int state, vaddr_t halt_pc, uint32_t halt_ret);
 
 static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
-  //*dest = ~*src1;
   rtl_li(&ir, 0);
   rtl_sub(&t1, &ir, src1);
   rtl_subi(dest, &t1, 1);
-}
-
-static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
-  // dest <- signext(src1[(width * 8 - 1) .. 0])
-  if (*src1 >> (width * 8 - 1) == 1)
-    switch(width) {
-      case 1: *dest = *src1 | 0xffffff00; break;
-      case 2: *dest = *src1 | 0xffff0000; break;
-      default: *dest = *src1;
-    }
-  else
-    *dest = *src1;
-  
-  /*if (width == 1) {
-    if (*src1 >> 7 == 1)
-      *dest = *src1 | 0xffffff00;
-    else
-      *dest = *src1 & 0x000000ff;
-  }
-  else if (width == 2) {
-    if (*src1 >> 15 == 1)
-      *dest = *src1 | 0xffff0000;
-    else
-      *dest = *src1 & 0x0000ffff;
-  }
-  else
-    *dest = *src1;*/
 }
 
 static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
@@ -175,6 +147,20 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
   rtl_shri(dest, src1, (width * 8 - 1));
   rtl_andi(dest, dest, 1);
+}
+
+static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
+  // dest <- signext(src1[(width * 8 - 1) .. 0])
+  rtl_msb(&t0, src1, width);
+  if (t0 == 1)
+    switch(width) {
+      case 4: rtl_mv(dest, src1); break;
+      case 2: rtl_ori(&t1, src1, 0xffff0000); rtl_mv(dest, &t1); break;
+      case 1: rtl_ori(&t1, src1, 0xffffff00); rtl_mv(dest, &t1); break;
+      default: assert(0);
+    }
+  else
+    rtl_mv(dest, src1);
 }
 
 static inline void rtl_mux(rtlreg_t* dest, const rtlreg_t* cond, const rtlreg_t* src1, const rtlreg_t* src2) {
