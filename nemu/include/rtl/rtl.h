c2adcb754e13412053042f53af720e364cba4265
tracer-ics2019 <tracer@njuics.org>
2019-11-01 20:19:45 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  20:19:45 up 2 days,  3:07,  5 users,  load average: 1.19, 0.85, 0.59 90c4f419fa3f084b14f6e0158fc59e7de39366e9

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -133,6 +133,9 @@ void interpret_rtl_exit(int state, vaddr_t halt_pc, uint32_t halt_ret);
 static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
   *dest = ~*src1;
+  rtl_li(&t1, 0);
+  rtl_sub(&t1, 0, &t1);
+  rtl_subi(dest, &t1, 1);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
@@ -170,7 +173,8 @@ static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  TODO();
+  rtl_shri(dest, src1, (width * 8 - 1));
+  rtl_andi(dest, dest, 1);
 }
 
 static inline void rtl_mux(rtlreg_t* dest, const rtlreg_t* cond, const rtlreg_t* src1, const rtlreg_t* src2) {
