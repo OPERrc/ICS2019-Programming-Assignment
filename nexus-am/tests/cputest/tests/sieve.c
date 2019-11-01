669dacfd2df5e7ce6cb6ab32cefa3eff713535f9
tracer-ics2019 <tracer@njuics.org>
2019-11-01 14:44:58 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  14:44:58 up 1 day, 21:32,  5 users,  load average: 0.94, 1.01, 0.97 c7e1a1c42f7fd7a8455ec8f6a1d3628497f32093

---
+++ nexus-am/tests/cputest/tests/sieve.c
@@ -0,0 +1,50 @@
+//#include <benchmark.h>
+#include "trap.h"
+
+static int N;
+
+static int ans;
+static uint32_t *primes;
+
+static inline int get(int n) {
+  return (primes[n >> 5] >> (n & 31)) & 1;
+}
+
+static inline void clear(int n) {
+  primes[n >> 5] &= ~(1ul << (n & 31));
+}
+
+void bench_sieve_prepare() {
+  N = 10000000;
+  primes = (uint32_t*)(N / 32);
+  for (int i = 0; i <= N / 32; i ++) {
+    primes[i] = 0xffffffff;
+  }
+}
+
+void bench_sieve_run() {
+  for (int i = 1; i <= N; i ++)
+    if (!get(i)) return;
+  for (int i = 2; i * i <= N; i ++) {
+    if (get(i)) {
+      for (int j = i + i; j <= N; j += i)
+        clear(j);
+    }
+  }
+  ans = 0;
+  for (int i = 2; i <= N; i ++) 
+    if (get(i)) {
+      ans ++;
+    }
+}
+
+int bench_sieve_validate() {
+  return ans == 0x000a2403;
+}
+
+int main() {
+  bench_sieve_prepare();
+  bench_sieve_run();
+  assert(bench_sieve_validate());
+  return 0;
+}
