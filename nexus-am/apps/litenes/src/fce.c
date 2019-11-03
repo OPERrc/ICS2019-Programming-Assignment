0251b7c77f478e415bc5fa9d83b660fae99d2300
tracer-ics2019 <tracer@njuics.org>
2019-11-03 11:56:11 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  11:56:11 up 3 days, 18:43,  7 users,  load average: 0.99, 0.86, 0.64 bb468d94b1091e60c12c8f17beb46db016eef54e

--- nexus-am/apps/litenes/src/fce.c
+++ nexus-am/apps/litenes/src/fce.c
@@ -131,13 +131,13 @@ void fce_update_screen() {
   int ypad = (h - H) / 2;
   assert(xpad >= 0 && ypad >= 0);
 
-  for (int x = 0; x < W; x ++) {
+  /*for (int x = 0; x < W; x ++) {
     for (int y = 0; y < H; y ++) {
       draw_rect(&canvas[y * W + x], xpad + x, ypad + y, 1, 1);
     }
   }
-  
-  //draw_rect(canvas, xpad, ypad, W, H);
+  */
+  draw_rect(canvas, xpad, ypad, W, H);
 
   draw_sync();
 
