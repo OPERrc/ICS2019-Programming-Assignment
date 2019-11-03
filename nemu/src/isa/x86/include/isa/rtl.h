cd59ed546496815889ef2b329866d38f10ae3798
tracer-ics2019 <tracer@njuics.org>
2019-11-03 22:46:39 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  22:46:39 up 4 days,  5:34,  7 users,  load average: 0.51, 0.47, 0.28 cf6a54a47d7ec308f122941a6e61a10dda0b9aca

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -81,7 +81,8 @@ static inline void rtl_is_add_overflow(rtlreg_t* dest,
 static inline void rtl_is_add_carry(rtlreg_t* dest,
     const rtlreg_t* res, const rtlreg_t* src1) {
   // dest <- is_carry(src1 + src2)
-  *dest = *res < *src1 ? 1 : 0;
+  // *dest = *res < *src1 ? 1 : 0;
+  rtl_setrelop(RELOP_LTU, dest, res, src1);
 }
 
 #define make_rtl_setget_eflags(f) \
