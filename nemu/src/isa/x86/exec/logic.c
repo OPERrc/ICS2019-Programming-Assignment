14ffccdf50d281b685d822410c9a180a7e5150b4
tracer-ics2019 <tracer@njuics.org>
2019-11-03 23:17:07 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  23:17:07 up 4 days,  6:04,  7 users,  load average: 0.99, 0.77, 0.62 649aa71971c39f9fc6a2d9123d2825e4571d7af

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -104,19 +104,18 @@ make_EHelper(rol) {
     // rtl_msb(&s1, &id_dest->val, id_dest->width);
     id_dest->val = id_dest->val * 2 + s1;
     cpu.CF = cpu.CF ^ s1;
-  }
+  }*/
+  rtl_shri(&s0, &id_dest->val, 32 - id_src->val);
+  rtl_shli(&id_dest->val, &id_dest->val, id_src->val);
+  rtl_or(&id_dest->val, &id_dest->val, &s0);
   operand_write(id_dest, &id_dest->val);
-  if (id_src->val == 1) {
+  
+  /*if (id_src->val == 1) {
     if (id_dest->val >> (id_dest->width * 8 - 1) != cpu.CF)
       cpu.OF = 1;
     else
       cpu.OF = 0;
   }*/
-
-  rtl_shri(&s0, &id_dest->val, 32 - id_src->val);
-  rtl_shli(&id_dest->val, &id_dest->val, id_src->val);
-  rtl_or(&id_dest->val, &id_dest->val, &s0);
-  operand_write(id_dest, &id_dest->val);
   rtl_setrelopi(RELOP_EQ, &s0, &id_src->val, 1);
   rtl_msb(&s1, &id_dest->val, id_dest->width);
   if (s0) {
