65c20b9b34f216baab59f0f409ac249bb4b97213
tracer-ics2019 <tracer@njuics.org>
2019-11-08 21:58:07 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  21:58:07 up 12:13,  3 users,  load average: 0.78, 0.50, 0.45 c453fffb3c9cd7e47d8e3443e8447dde37613f7

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -9,7 +9,7 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   cpu.TF = 0;
   rtl_push(&cpu.cs);
   rtl_push(&ret_addr);
-  Log("0x%x\n", ret_addr);
+  // Log("0x%x\n", ret_addr);
   s0 = vaddr_read(cpu.idtr.base + NO * 8, 2);
   s1 = vaddr_read(cpu.idtr.base + NO * 8 + 6, 2);
   s1 = s1 << 16 | s0;
