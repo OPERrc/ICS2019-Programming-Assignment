6bd4e10f9ace31d0d3d5167e1401ec13e4692e8d
tracer-ics2019 <tracer@njuics.org>
2019-11-08 16:34:23 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  16:34:23 up  6:49,  3 users,  load average: 1.10, 0.55, 0.57 d7386e4f40366805b0126a8575ea3f1badc3ab57

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -4,6 +4,9 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+  rtl_push(&cpu.eflags);
+  rtl_push(&cpu.cs);
+  rtl_push(&cpu.pc);
   cpu.pc = vaddr_read(NO, 4);
 }
 
