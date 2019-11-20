7f13ab4cefb9c77a891372a6f566cef1f06b1b2c
tracer-ics2019 <tracer@njuics.org>
2019-11-20 10:20:18 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  10:20:18 up  2:08,  1 user,  load average: 0.30, 0.38, 0.42 8fbdad2d6dd8c4b7f0cfc9cfc5f8cad0f6cec9fa

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -18,6 +18,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   // read ehdr
   size_t point = 0;
+  printf("%s\n", "Loading");
   printf("%d\n", point);
   point += ramdisk_read(&ehdr->e_ident, point, sizeof(ehdr->e_ident));
   printf("%d\n", point);
