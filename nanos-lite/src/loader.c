c3ac6c63eddd2ae29a5bde85dad26c12439dce4d
tracer-ics2019 <tracer@njuics.org>
2019-11-20 15:37:08 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  15:37:08 up  7:25,  3 users,  load average: 0.92, 0.83, 0.78 bf675d6cddff443efe845d04c460a32ce247781a

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -27,8 +27,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       // size_t data[phdr.p_filesz];
       // ramdisk_read(&data, phdr.p_offset, phdr.p_filesz);
       uint32_t *fb = (uint32_t *)(uintptr_t)phdr.p_vaddr;
-      memcpy(fb, &phdr.p_offset, phdr.p_filesz);
-      memset(&fb[phdr.p_filesz], 0, phdr.p_memsz - phdr.p_filesz);
+      ramdisk_read(fb, phdr.p_offset, phdr.p_filesz);
+      memset(fb, 0, phdr.p_memsz - phdr.p_filesz);
       // ramdisk_write(&data, phdr.p_vaddr, phdr.p_memsz);
     }
   }
