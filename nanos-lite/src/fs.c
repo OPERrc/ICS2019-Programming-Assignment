5537a968040db6860267065798a2edb1ddba2830
tracer-ics2019 <tracer@njuics.org>
2019-11-21 15:13:40 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  15:13:40 up 1 day,  7:01,  5 users,  load average: 0.66, 0.37, 0.40 2f3114f4dfc00ca7d86208d4e45a2512d5a612ba

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -62,7 +62,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
   if (fd >= NR_FILES)
     return -1;
   
-  ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   fs_lseek(fd, len, SEEK_CUR);
   return len;
 }
@@ -70,7 +70,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
 size_t fs_write(int fd, const void *buf, size_t len) {
   assert(file_table[fd].open_offset <= file_table[fd].size);
   if (fd != 1 && fd != 2) {
-    ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
     fs_lseek(fd, len, SEEK_CUR);
     return len;
   }
