03b1affa10c9b65a2e408e84eff6c62298f458bd
tracer-ics2019 <tracer@njuics.org>
2019-11-21 13:55:16 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  13:55:16 up 1 day,  5:43,  5 users,  load average: 0.47, 0.50, 0.57 44780adff333fefe9a604fd724b0ba66c1c03eaa

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -61,7 +61,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
     return -1;
   
   ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-  fs_lseek(fd, len, SEEK_CUR);
+  // fs_lseek(fd, len, SEEK_CUR);
   return len;
 }
 
