e91bc8dcfbe7fb7ebb1b28ecce04f84096482e3e
tracer-ics2019 <tracer@njuics.org>
2019-11-24 16:40:35 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  16:40:35 up  1:10,  1 user,  load average: 0.65, 0.40, 0.38 220f4400c63cc4dd1747b4b92bd1bf94b949b745

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,7 +22,6 @@ static const char *keyname[256] __attribute__((used)) = {
 size_t events_read(void *buf, size_t offset, size_t len) {
   int key = read_key();
   char name[128];
-  char time[128];
   int down = 0;
   int num = 0;
   if (key & 0x8000) {
@@ -30,13 +29,11 @@ size_t events_read(void *buf, size_t offset, size_t len) {
     down = 1;
   }
   if (key != _KEY_NONE) {
-    sprintf(name, "%s\n", name);
     if (down) 
       sprintf(name, "kd %s\n", keyname[key]);
     else
       sprintf(name, "ku %s\n", keyname[key]);
     num = 0;
-    printf("name after = %s\n", name);
     while (name[num] && num < len) {
       *(char *)buf++ = name[num];
       num++;
@@ -44,9 +41,9 @@ size_t events_read(void *buf, size_t offset, size_t len) {
     return num;
   } else {
     num = 0;
-    sprintf(time, "t %d\n", uptime());
-    while (time[num] && num < len) {
-      *(char *)buf++ = time[num];
+    sprintf(name, "t %d\n", uptime());
+    while (name[num] && num < len) {
+      *(char *)buf++ = name[num];
       num++;
     }
     return num;
