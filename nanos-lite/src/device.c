ff860022ffdc2f2598f91574d403d1791fbcc48d
tracer-ics2019 <tracer@njuics.org>
2019-11-24 15:55:53 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  15:55:53 up 25 min,  1 user,  load average: 0.72, 0.78, 0.68 f48dcaf740d56c3f6bfb519df29fb920225a01ff

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -31,9 +31,9 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   }
   if (key != _KEY_NONE) {
     if (down) 
-      sprintf(name, "kd %s\n", keyname[key]);
-    else
       sprintf(name, "k %s\n", keyname[key]);
+    else
+      sprintf(name, "ku %s\n", keyname[key]);
     num = 0;
     while (name[num] && num < len) {
       *(char *)buf++ = name[num];
