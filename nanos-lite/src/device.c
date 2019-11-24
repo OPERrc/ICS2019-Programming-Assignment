9cf8ee8356f653c96c0429789b51b318f89d0db8
tracer-ics2019 <tracer@njuics.org>
2019-11-24 11:17:16 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  11:17:16 up  2:24,  3 users,  load average: 0.57, 0.86, 0.79 8a16351ddf8c7f504966b305d7661f0bbdccc3e1

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -26,8 +26,13 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  memcpy(buf, &dispinfo[offset], len);
-  return len;
+  size_t num = 0;
+  while (buf && num < len) {
+    *(char *)buf++ = dispinfo[offset + num];
+    num++;
+  }
+  // memcpy(buf, &dispinfo[offset], len);
+  return num;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
