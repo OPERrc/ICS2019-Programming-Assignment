de87a69566edde3877ba9a6b83e5c42c80c4af96
tracer-ics2019 <tracer@njuics.org>
2019-11-24 11:14:56 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  11:14:56 up  2:22,  3 users,  load average: 0.89, 1.00, 0.82 4c7ce4a3c496cc802c0c68339bfcc829ba6ad250

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -74,6 +74,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
   // printf("name = %s\n", file_table[fd].name);
   // printf("file_table[fd].read == NULL? %d\n", file_table[fd].read == NULL);
   if (file_table[fd].read == NULL) {
+    printf("here\n");
     size_t off = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
     // printf("open_offset_before = %d\n", file_table[fd].open_offset);
     fs_lseek(fd, off, SEEK_CUR);
