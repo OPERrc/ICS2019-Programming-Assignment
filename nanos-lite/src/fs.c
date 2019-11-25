e488d3d27102b97630d62d129043004aca1b48de
tracer-ics2019 <tracer@njuics.org>
2019-11-25 17:10:29 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  17:10:29 up 1 day,  1:40,  3 users,  load average: 1.64, 1.54, 1.48 91eb5202b6ad45d6c238f593ecc15cc01ee6ce17

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -40,7 +40,7 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stderr", 0, 0, invalid_read, serial_write},
   {"/dev/fb", 0, 0, invalid_read, fb_write},
   {"/dev/events", 0, 0, events_read, invalid_write},
-  {"/proc/dispinfo", 0, 128, dispinfo_read, invalid_write},
+  {"/proc/dispinfo", 128, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
 
