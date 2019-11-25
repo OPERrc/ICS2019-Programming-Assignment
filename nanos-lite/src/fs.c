91565415507c0336b9cdd8466c16ceb15f9f9a9e
tracer-ics2019 <tracer@njuics.org>
2019-11-25 17:08:28 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  17:08:28 up 1 day,  1:38,  3 users,  load average: 1.69, 1.49, 1.46 9ca8f8de9a3a01921df40ed450793136a226d189

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -90,6 +90,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
   }
   else {
     size_t off = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    printf("%d\n", off);
     fs_lseek(fd, off, SEEK_CUR);
     return off;
   }
