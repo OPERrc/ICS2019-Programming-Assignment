b66d15594d15caf9cb75b786b058c6f53e771ff2
tracer-ics2019 <tracer@njuics.org>
2019-11-28 17:27:03 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  17:27:03 up 2 days, 19:51,  1 user,  load average: 0.96, 1.03, 0.82 c852cd4ee06012f37eb7aa6bfdab54e6bce03981

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -82,8 +82,6 @@ size_t fs_read(int fd, void *buf, size_t len) {
     return -1;
   }
   // printf("name = %s\n", file_table[fd].name);
-  if (len + file_table[fd].open_offset > file_table[fd].size)
-    len = file_table[fd].size - file_table[fd].open_offset;
   if (file_table[fd].read == NULL) {
     size_t off = 0;
     if (file_table[fd].open_offset + len > file_table[fd].size)
@@ -103,9 +101,9 @@ size_t fs_read(int fd, void *buf, size_t len) {
 
 size_t fs_write(int fd, const void *buf, size_t len) {
   assert(file_table[fd].open_offset <= file_table[fd].size);
-  if (len + file_table[fd].open_offset > file_table[fd].size)
-    len = file_table[fd].size - file_table[fd].open_offset;
   if (file_table[fd].write == NULL) {
+    if (len + file_table[fd].open_offset > file_table[fd].size)
+      len = file_table[fd].size - file_table[fd].open_offset;
     ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
     fs_lseek(fd, len, SEEK_CUR);
     return len;
