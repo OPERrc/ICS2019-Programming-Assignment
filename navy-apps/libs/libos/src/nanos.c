875f13753ad18d3ca3aa5ee4fd4a22b8cb5b63cf
tracer-ics2019 <tracer@njuics.org>
2019-11-28 17:53:05 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  17:53:05 up 2 days, 20:17,  3 users,  load average: 0.76, 0.56, 0.55 58c821697739dfe7e849ab12d525b63d5f264cc1

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -96,7 +96,7 @@ off_t _lseek(int fd, off_t offset, int whence) {
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
-  printf("filename = %s\n", fname;
+  printf("filename = %s\n", fname);
   return _syscall_(SYS_execve, (intptr_t)fname, (intptr_t)argv, (intptr_t)envp);
 }
 
