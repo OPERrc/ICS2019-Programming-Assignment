e49904a29f7485388558a83cad5611fcd33988d2
tracer-ics2019 <tracer@njuics.org>
2019-11-28 17:51:20 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  17:51:20 up 2 days, 20:15,  3 users,  load average: 0.42, 0.49, 0.53 d019b3718df99e1f8b8ffc9137c9cd49f6fb35ea

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,13 @@
 #include "common.h"
 #include "syscall.h"
+#include "proc.h"
 
 int fs_open(const char *pathname, int flags, int mode);
 size_t fs_read(int fd, void *buf, size_t len);
 size_t fs_write(int fd, const void *buf, size_t len);
 size_t fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
+void naive_uload(PCB *pcb, const char *filename);
 
 void sys_yield(_Context *c) {
   _yield();
@@ -54,35 +56,25 @@ void sys_close(_Context *c) {
   c->GPRx = fs_close(c->GPR2);
 }
 
+void sys_execve(_Context *c) {
+  naive_uload(NULL, (char *)c->GPR2);
+  c->GPRx = -1;
+}
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    case SYS_exit:
-      sys_exit(c);
-      break;
-    case SYS_yield:
-      sys_yield(c);
-      break;
-    case SYS_write:
-      sys_write(c);
-      break;
-    case SYS_brk:
-      sys_brk(c);
-      break;
-    case SYS_open:
-      sys_open(c);
-      break;
-    case SYS_close:
-      sys_close(c);
-      break;
-    case SYS_lseek:
-      sys_lseek(c);
-      break;
-    case SYS_read:
-      sys_read(c);
-      break;
+    case SYS_exit: sys_exit(c); break;
+    case SYS_yield: sys_yield(c); break;
+    case SYS_write: sys_write(c); break;
+    case SYS_brk: sys_brk(c); break;
+    case SYS_open: sys_open(c); break;
+    case SYS_close: sys_close(c); break;
+    case SYS_lseek: sys_lseek(c); break;
+    case SYS_read: sys_read(c); break;
+    case SYS_execve: sys_execve(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
