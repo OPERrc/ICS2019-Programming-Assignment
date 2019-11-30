125b63ba497098460ce15e0812a1e593e1d18381
tracer-ics2019 <tracer@njuics.org>
2019-11-30 18:30:53 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  18:30:53 up 4 days, 20:55,  3 users,  load average: 0.73, 0.72, 0.31 d35b3f158eda0c13f6ff759a286fc5f336451855

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -38,7 +38,7 @@ char *my_itoa(int value, char *str, int radix) {
 int printf(const char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
-  char buf[128];
+  char buf[1000];
   char *str = buf;
   vsprintf(str, fmt, ap);
   va_end(ap);
@@ -53,7 +53,7 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  char a[128] = "";
+  char a[1000] = "";
   char *str = a;
   char *tmp = out;
   int d, count = 0;
