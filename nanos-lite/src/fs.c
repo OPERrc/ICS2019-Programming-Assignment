1eb2067ce4f15bdaac762ad499edab6a9cec2834
tracer-ics2019 <tracer@njuics.org>
2019-12-07 11:50:56 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  11:50:56 up 11 days, 14:15,  4 users,  load average: 1.00, 0.81, 0.76 700e09e850d0b5bd6a409a3c79a18a1c16b45cfb

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -3,6 +3,7 @@
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
+
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t dispinfo_read(void *buf, size_t offset, size_t len);
 size_t events_read(void *buf, size_t offset, size_t len);
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -24,14 +24,14 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   // read ehdr
   int fd = fs_open(filename, 0, 0);
-  fs_read(fd, &ehdr, sizeof(ehdr));
+  fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
   int point = ehdr.e_phoff;
 
   for (size_t i = 0; i < ehdr.e_phnum; i++) {
     // read phdr
     point += fs_read(fd, &phdr, ehdr.e_phentsize);
     if (phdr.p_type == PT_LOAD) {
-      uintptr_t *mem = (uintptr_t *)phdr.p_vaddr;
+      char *mem = (char *)phdr.p_vaddr;
       fs_lseek(fd, phdr.p_offset, SEEK_SET);
       fs_read(fd, mem, phdr.p_filesz);
       fs_lseek(fd, point, SEEK_SET);
