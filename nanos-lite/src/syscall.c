1a24a302c6a78fd552df88a8b2d12c748b2c87be
tracer-ics2019 <tracer@njuics.org>
2019-12-07 09:04:29 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  09:04:29 up 11 days, 11:29,  3 users,  load average: 0.05, 0.36, 0.52 71094530cd275eb43b7444c05faf3caca9d943b6

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -21,7 +21,7 @@ void sys_execve(_Context *c, const char *fname) {
 }
 
 void sys_exit(_Context *c) {
-  if (c->GPR2 == 0)
+  //if (c->GPR2 == 0)
     sys_execve(c, "/bin/init");
   _halt(c->GPR2);
 }
