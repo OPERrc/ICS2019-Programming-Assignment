ab49cfa20ace8408ad875069c483d2ad1ab7c4f3
tracer-ics2019 <tracer@njuics.org>
2019-12-15 07:59:41 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  07:59:41 up 1 day, 21:49,  1 user,  load average: 0.53, 0.37, 0.24 c6ea50c33afa6600dda45a70a27ea8d6c60313e1

--- nexus-am/am/src/x86/nemu/vme.c
+++ nexus-am/am/src/x86/nemu/vme.c
@@ -36,24 +36,24 @@ int _vme_init(void* (*pgalloc_f)(size_t), void (*pgfree_f)(void*)) {
     for (; pdir_idx < pdir_idx_end; pdir_idx ++) {
       // fill PDE
       kpdirs[pdir_idx] = (uintptr_t)ptab | PTE_P;
-      printf("kpdirs[pdir_idx]? kpdirs[%d] = %x\n", pdir_idx, kpdirs[pdir_idx]);
+      printf("kpdirs[pdir_idx]? kpdirs[%d] = 0x%x\n", pdir_idx, kpdirs[pdir_idx]);
 
       // fill PTE
       PTE pte = PGADDR(pdir_idx, 0, 0) | PTE_P;
-      printf("pte? = %x\n", pte);
+      printf("pte? = 0x%x\n", pte);
       PTE pte_end = PGADDR(pdir_idx + 1, 0, 0) | PTE_P;
-      printf("pte_end? = %x\n", pte_end);
+      printf("pte_end? = 0x%x\n", pte_end);
       for (; pte < pte_end; pte += PGSIZE) {
         *ptab = pte;
         ptab ++;
       }
     }
   }
-
+  assert(0);
   set_cr3(kpdirs);
   set_cr0(get_cr0() | CR0_PG);
   vme_enable = 1;
-  assert(0);
+  
   return 0;
 }
 
