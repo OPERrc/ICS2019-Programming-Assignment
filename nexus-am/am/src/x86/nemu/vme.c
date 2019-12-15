8d29fcabeb6b26a3db2049b6eee9b5c29c84bcd7
tracer-ics2019 <tracer@njuics.org>
2019-12-15 13:01:25 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  13:01:25 up 2 days,  2:51,  2 users,  load average: 0.66, 0.40, 0.33 844bb16b82a53f6ff78d142ed33fd7c4296ce8d1

--- nexus-am/am/src/x86/nemu/vme.c
+++ nexus-am/am/src/x86/nemu/vme.c
@@ -100,11 +100,16 @@ int _map(_AddressSpace *as, void *va, void *pa, int prot) {
   v_addr.val = (uint32_t)va;
   
   PDE *updir = (PDE *)as->ptr;
+  printf("updir = 0x%x\n", updir);
+  printf("updir[v_addr.dir] = 0x%x\n", updir[v_addr.dir]);
   if (updir[v_addr.dir] == 0)
     updir[v_addr.dir] = ((uint32_t)(pgalloc_usr(1)) << 12) | 0x001;
-  
+  printf("updir[v_addr.dir] = 0x%x\n", updir[v_addr.dir]);
   PTE *uptabs = (PDE *)(updir[v_addr.dir]);
+  printf("uptabs = 0x%x\n", uptabs);
+  printf("uptabs[v_addr.page] = 0x%x\n", uptabs[v_addr.page]);
   uptabs[v_addr.page] = ((uint32_t)(pa - v_addr.offset) << 12) | 0x001;
+  printf("uptabs[v_addr.page] = 0x%x\n", uptabs[v_addr.page]);
   //*(PDE *)(as->ptr + v_addr.dir * 4) = ;
   //*(PDE *)(as->ptr + v_addr.dir * 4) = ;
   //PTE *p_addr = (PTE *)(pa - v_addr.offset);
