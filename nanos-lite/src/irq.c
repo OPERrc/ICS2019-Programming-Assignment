6f7be215449b67759c860716c584532a3086590f
tracer-ics2019 <tracer@njuics.org>
2019-12-15 23:03:26 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  23:03:26 up 2 days, 12:53,  2 users,  load average: 0.63, 0.61, 0.44 cf7f3d6ea038621cea6c162a208d69a4e65494a6

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,16 +4,20 @@ _Context* do_syscall(_Context*);
 _Context* schedule(_Context *prev);
 
 static _Context* do_event(_Event e, _Context* c) {
-  _Context *next;
+  _Context *next = c;
   switch (e.event) {
     case _EVENT_YIELD: 
       //printf("Event \"YIELD\" catched!\n"); 
-      next =  schedule(c);
+      next = schedule(c);
       break;
     case _EVENT_SYSCALL: 
       // printf("Event \"SYSCALL\" catched!\n"); 
       next = do_syscall(c);
       break; 
+    case _EVENT_IRQ_TIMER:
+      _yield();
+      Log("Event \"IRQ_TIMER\" catched!\n");
+      break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
