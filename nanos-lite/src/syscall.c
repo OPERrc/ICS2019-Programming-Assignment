a04bdad8159b10c39917c23c0969f0a40a0f0811
tracer-ics2019 <tracer@njuics.org>
2019-12-15 20:03:27 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  20:03:27 up 2 days,  9:53,  2 users,  load average: 0.44, 0.46, 0.46 24b5423288d49acbfd9e062aab9eff88f5512373

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -53,8 +53,8 @@ void sys_brk(_Context *c) {
 
   if (current->max_brk == 0) {
     current->max_brk = cur_brk;
-    void *p_mem = new_page(1);
-    _map(&current->as, (void *)(cur_brk & ~0xfff), p_mem, 0);
+    //void *p_mem = new_page(1);
+    //_map(&current->as, (void *)(cur_brk & ~0xfff), p_mem, 0);
     //if (!has_map(cur_brk))
   }
   else
