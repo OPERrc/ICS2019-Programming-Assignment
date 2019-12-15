415c0ec238a91ddee7c0d4a90ce5f5547676f7ab
tracer-ics2019 <tracer@njuics.org>
2019-12-15 14:22:54 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  14:22:54 up 2 days,  4:12,  2 users,  load average: 1.09, 0.64, 0.37 9844af602779c7d26033f8336175a2f4797b40c5

--- nexus-am/am/src/x86/nemu/vme.c
+++ nexus-am/am/src/x86/nemu/vme.c
@@ -127,12 +127,12 @@ int _map(_AddressSpace *as, void *va, void *pa, int prot) {
 
 _Context *_ucontext(_AddressSpace *as, _Area ustack, _Area kstack, void *entry, void *args) {
   _Context *new = ustack.end - 0x100 - sizeof(_Context);
-  printf("as->ptr = 0x%x\n", as->ptr);
   new->eip = (uintptr_t)entry;
   new->cs = 8;
   new->as = as;
   void Context_display(_Context *c);
-  Context_display(new);
+  //Context_display(new);
+  printf("as->ptr = 0x%x\n", new->as->ptr);
   printf("--------------------------------\n");
   printf("in vme.c: _ucontext:\n");
   printf("_Context may have overlap bugs!\n");
