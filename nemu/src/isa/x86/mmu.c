5b61645b0f18f5986d25dda235e73e5d531ae377
tracer-ics2019 <tracer@njuics.org>
2019-12-15 14:44:50 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  14:44:50 up 2 days,  4:34,  2 users,  load average: 0.80, 0.67, 0.54 8d7cdebf8651754b18bced87ad617630881d6266

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -25,7 +25,8 @@ paddr_t page_translate(vaddr_t addr) {
   PTE pte;
 
   linear_addr.addr = addr;
-  //printf("linear_addr = 0x%x\n", linear_addr.addr);
+  printf("------------------\n");
+  printf("linear_addr = 0x%x\n", linear_addr.addr);
   //printf("dir = 0x%x\n", linear_addr.dir);
   //printf("page = 0x%x\n", linear_addr.page);
   //printf("offset = 0x%x\n", linear_addr.offset);
@@ -44,12 +45,12 @@ paddr_t page_translate(vaddr_t addr) {
     print_debug_info
     printf("pte = 0x%x\n", pte.val);
   }
-  printf("pte = 0x%x\n", pte.val);
+  //printf("pte = 0x%x\n", pte.val);
   assert(pte.present);
 
   //panic("untested codes here!\n");
   printf("paddr = 0x%x\n", (pte.page_frame << 12) + linear_addr.offset);
-  //printf("paddr_read = 0x%x\n", paddr_read((pte.page_frame << 12) + linear_addr.offset, 4));
+  printf("paddr_read = 0x%x\n", paddr_read((pte.page_frame << 12) + linear_addr.offset, 4));
   return (pte.page_frame << 12) + linear_addr.offset;
 }
 
