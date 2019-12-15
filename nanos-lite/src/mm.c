66845fcf303bb0f30ddda5a027cf4d82da45ffce
tracer-ics2019 <tracer@njuics.org>
2019-12-15 20:05:35 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  20:05:35 up 2 days,  9:55,  2 users,  load average: 0.92, 0.58, 0.50 2c1de8c6de7360a9e279d6ba62d866a2cdfea9c

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -20,6 +20,7 @@ int mm_brk(uintptr_t brk, intptr_t increment) {
     return 0;
   void *v_brk = (void *)((brk + PGSIZE) & 0xfff);
   while ((uintptr_t)v_brk <= brk + increment) {
+    printf("v_brk = 0x%x\n", v_brk);
     void *p_mem = new_page(1);
     _map(&current->as, v_brk, p_mem, 0);
     v_brk += PGSIZE;
