e9c684c0daee7e743e33efc8db265e17b955d740
tracer-ics2019 <tracer@njuics.org>
2019-12-15 08:43:02 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  08:43:02 up 1 day, 22:32,  2 users,  load average: 0.50, 0.40, 0.34 e172b72ad54a289b611bcbb1bb1d480ed3d45c68

--- nexus-am/am/src/x86/nemu/vme.c
+++ nexus-am/am/src/x86/nemu/vme.c
@@ -37,7 +37,7 @@ int _vme_init(void* (*pgalloc_f)(size_t), void (*pgfree_f)(void*)) {
     for (; pdir_idx < pdir_idx_end; pdir_idx ++) {
       // fill PDE
       kpdirs[pdir_idx] = (uintptr_t)ptab | PTE_P;
-      //printf("kpdirs[pdir_idx]? kpdirs[%d] = 0x%x\n", pdir_idx, kpdirs[pdir_idx]);
+      printf("kpdirs[pdir_idx]? kpdirs[%d] = 0x%x\n", pdir_idx, kpdirs[pdir_idx]);
 
       // fill PTE
       PTE pte = PGADDR(pdir_idx, 0, 0) | PTE_P;
