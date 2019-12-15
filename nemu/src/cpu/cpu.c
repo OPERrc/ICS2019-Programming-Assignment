8406750ee586333f4a4c3b3599b8af8e5c1233e4
tracer-ics2019 <tracer@njuics.org>
2019-12-15 16:44:35 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  16:44:35 up 2 days,  6:34,  2 users,  load average: 0.49, 0.75, 0.78 ef904ca7db6932997a68cde57d831f938bc6388

--- nemu/src/cpu/cpu.c
+++ nemu/src/cpu/cpu.c
@@ -14,7 +14,7 @@ void decinfo_set_jmp(bool is_jmp) {
 void isa_exec(vaddr_t *pc);
 
 vaddr_t exec_once(void) {
-  printf("cpu.pc = 0x%x\n", cpu.pc);
+  //printf("cpu.pc = 0x%x\n", cpu.pc);
   decinfo.seq_pc = cpu.pc;
   isa_exec(&decinfo.seq_pc);
   update_pc();
