b5c58db8f36d05b12c0d46910a5c1872ad33168e
tracer-ics2019 <tracer@njuics.org>
2019-12-15 08:38:52 +0800

    >  compile 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  08:38:52 up 1 day, 22:28,  2 users,  load average: 0.43, 0.36, 0.32 41cf6bce6dce8fd7f418765d11c2e357bf9b8b7e

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -29,6 +29,7 @@ paddr_t page_translate(vaddr_t addr) {
 
   //panic("untested codes here!\n");
   printf("paddr = 0x%x\n", (pte.page_frame << 12) + linear_addr.offset * 4);
+  printf("paddr_read = 0x%x\n", paddr_read((pte.page_frame << 12) + linear_addr.offset * 4, 4));
   return (pte.page_frame << 12) + linear_addr.offset * 4;
 }
 
