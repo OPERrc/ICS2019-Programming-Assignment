902ff61739649e04c85daac6c3cd34cec92e83ec
tracer-ics2019 <tracer@njuics.org>
2019-12-15 08:05:43 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  08:05:43 up 1 day, 21:55,  1 user,  load average: 0.10, 0.20, 0.19 1d6f572cda0e4803b39ed421fb7d76a39871eca3

--- nexus-am/am/src/x86/nemu/vme.c
+++ nexus-am/am/src/x86/nemu/vme.c
@@ -21,7 +21,7 @@ static _Area segments[] = {      // Kernel memory mappings
 int _vme_init(void* (*pgalloc_f)(size_t), void (*pgfree_f)(void*)) {
   pgalloc_usr = pgalloc_f;
   pgfree_usr = pgfree_f;
-
+printf("kpdirs = 0x%x\n", kpdirs);
   int i;
 
   // make all PDEs invalid
@@ -45,7 +45,7 @@ int _vme_init(void* (*pgalloc_f)(size_t), void (*pgfree_f)(void*)) {
       printf("pte_end? = 0x%x\n", pte_end);
       for (; pte < pte_end; pte += PGSIZE) {
         *ptab = pte;
-        ptab ++;
+        ptab ++;assert(0);
       }
     }
   }
