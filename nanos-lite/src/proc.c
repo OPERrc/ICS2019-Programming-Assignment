fc72a156bb2a9ae616910108b51f22c6bacc6a70
tracer-ics2019 <tracer@njuics.org>
2019-12-15 20:35:52 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  20:35:52 up 2 days, 10:25,  2 users,  load average: 0.59, 0.62, 0.63 afcc12e904b5216506169b058097f54d80c1a6

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -23,7 +23,7 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  context_uload(&pcb[0], "/bin/text");
+  context_uload(&pcb[0], "/bin/hello");
   //context_uload(&pcb[1], "/bin/hello");
   //switch_boot_pcb();
   switch_boot_pcb();
