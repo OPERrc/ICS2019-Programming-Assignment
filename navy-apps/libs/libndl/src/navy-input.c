f88a554965d346102df5c97fb41b4912fba6d2b2
tracer-ics2019 <tracer@njuics.org>
2019-12-16 16:18:20 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  16:18:20 up 3 days,  6:08,  2 users,  load average: 1.15, 1.04, 1.01 13b7f9eecabe222c708fb7056a01d57f9ac306db

--- navy-apps/libs/libndl/src/navy-input.c
+++ navy-apps/libs/libndl/src/navy-input.c
@@ -1,14 +1,55 @@
 #include <ndl.h>
 #include <stdio.h>
-//#include <stddef.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
 
-int NDL_WaitEvent(NDL_Event *event);
+extern FILE *evtdev;
+
+#define keyname(k) #k,
+
+static const char *keys[] = {
+  "NONE",
+  _KEYS(keyname)
+};
+
+#define numkeys ( sizeof(keys) / sizeof(keys[0]) )
+
+int NDL_WaitKeyboard(NDL_Event *event) {
+  char buf[256], *p = buf, ch;
+
+  while (1) {
+    while ((ch = getc(evtdev)) != -1) {
+      *p ++ = ch;
+      assert(p - buf < sizeof(buf));
+      if (ch == '\n') break;
+    }
+
+    if (buf[0] == 'k') {
+      char keyname[32];
+      event->type = buf[1] == 'd' ? NDL_EVENT_KEYDOWN : NDL_EVENT_KEYUP;
+      event->data = -1;
+      sscanf(buf + 3, "%s", keyname);
+      for (int i = 0; i < numkeys; i ++) {
+        if (strcmp(keys[i], keyname) == 0) {
+          event->data = i;
+          break;
+        }
+      }
+      assert(event->data >= 1 && event->data < numkeys);
+      return 0;
+    }
+  }
+
+  assert(0);
+  return -1;
+}
 
 size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       NDL_Event e;
-      NDL_WaitEvent(&e);
+      NDL_WaitKeyboard(&e);
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
       //assert(0);
       if (e.type == NDL_EVENT_KEYDOWN || e.type == NDL_EVENT_KEYUP) {
