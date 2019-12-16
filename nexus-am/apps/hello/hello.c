3dd0ce84a4e269da76d112e1fd836a44be23dc0b
tracer-ics2019 <tracer@njuics.org>
2019-12-16 16:44:27 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  16:44:27 up 3 days,  6:34,  2 users,  load average: 0.78, 0.91, 0.95 7d1f04e9b34f59c53645f35a3d86d4959935023f

--- nexus-am/apps/hello/hello.c
+++ nexus-am/apps/hello/hello.c
@@ -1,12 +1,17 @@
 #include <am.h>
+#include <stdio.h>
 
 static inline void putstr(const char *s) {
   for (; *s; s ++) _putc(*s);
 }
 
 int main() {
+  
+  putstr("Hello, AM World @ " __ISA__ "\n");
+  int i = 2;
   while(1) {
-    putstr("Hello, AM World @ " __ISA__ "\n");
+    putstr("Hello, AM World @ " __ISA__);
+    printf("for the %d times!\n", i);
   }
   return 0;
 }
