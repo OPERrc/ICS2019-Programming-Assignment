a8eca158836d5579c163b4fe96a0350217cd9bee
tracer-ics2019 <tracer@njuics.org>
2019-12-16 16:03:53 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  16:03:53 up 3 days,  5:53,  2 users,  load average: 1.20, 1.13, 0.95 f499f1912557c1faf76e9e79e336767b5b956235

--- navy-apps/libs/libndl/src/navy-input.c
+++ navy-apps/libs/libndl/src/navy-input.c
@@ -9,7 +9,7 @@ size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
       NDL_Event e;
       NDL_WaitEvent(&e);
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      assert(0);
+      //assert(0);
       if (e.type == NDL_EVENT_KEYDOWN || e.type == NDL_EVENT_KEYUP) {
         kbd->keydown = e.type;
         kbd->keycode = e.data;
@@ -18,6 +18,7 @@ size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
         kbd->keydown = NDL_EVENT_KEYUP;
         kbd->keycode = NDL_SCANCODE_NONE;
       }
+      printf("key = %d, keydown = %d\n", kbd->keydown, kbd->keycode);
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }
