ef854d88f489284d84297cb14a60d25659ba2d05
tracer-ics2019 <tracer@njuics.org>
2019-12-21 16:54:02 +0800

    >  run 181220041 operrc Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  16:54:02 up 8 days,  6:43,  2 users,  load average: 1.12, 1.00, 0.75 92ebc4d88c0469253d4a6415cbd2725f450a4f1b

--- nexus-am/am/src/x86/nemu/vme.c
+++ nexus-am/am/src/x86/nemu/vme.c
@@ -139,8 +139,8 @@ int _map(_AddressSpace *as, void *va, void *pa, int prot) {
   PDE *updir = (PDE *)as->ptr;
   //printf("updir = 0x%x\n", updir);
   //printf("updir[v_addr.dir] = 0x%x\n", updir[v_addr.dir]);
-  if ((updir[v_addr.dir] & PTE_P) == 0)
-    updir[v_addr.dir] = (uint32_t)(pgalloc_usr(1)) | 0x001;
+  if ((updir[v_addr.dir] & PTE_P) == 0 || (updir[v_addr.dir] & PTE_A) == 0)
+    updir[v_addr.dir] = (uint32_t)(pgalloc_usr(1)) | PTE_P | PTE_A;
   //printf("updir[v_addr.dir] = 0x%x\n", updir[v_addr.dir]);
   PTE *uptabs = (PDE *)(updir[v_addr.dir] & ~0xfff);
   //printf("uptabs = 0x%x\n", uptabs);
